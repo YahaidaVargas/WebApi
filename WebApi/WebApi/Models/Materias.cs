2c8b3ab43aa8480c0f6fc7cdb73e98f2b9c3d41d
YahaidaVargas <lafenix2003@gmail.com>
2016-11-07 23:01:08 -0400

    tarea de la web api esa

---
+++ WebApi/WebApi/Models/Materias.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace WebApi.Models
+{
+    public class Materias
+    {
+        public int Id { get; set; }
+        public string Materia { get; set; }
+        public string Curso { get; set; }
+    }
+
+    class MateriasDbContext : DbContext {
+        public DbSet<Materias> Materias { get; set; }
+    }
+}
